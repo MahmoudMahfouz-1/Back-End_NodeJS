2a67cad1df5c0340824a23ec5933db27b7e625da
kareem-mahfouz1 <kareemmahfouz01@gmail.com>
2024-09-05 20:45:16 +0300

    global.js File added

---
+++ NodeJS_Crash_Course/Lesson2_NodeJS_Basics/global.js
@@ -0,0 +1,9 @@
+// The Global Variable in NodeJS is global not window like JS
+
+console.log(global);
+
+// we can use methods like setInterval, setTimeout and clearInterval 
+// Also some new methods like __dirname & __filename
+
+console.log(__filename);
+console.log(__dirname);
